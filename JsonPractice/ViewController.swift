a9fa48dd0059111b40028e9c89c0d0fc265700a3
Murali <kesani.murali@achiever-68.local>
2019-07-17 11:23:50 +0530

    Initiated with Json Get

--- JsonPractice/ViewController.swift
+++ JsonPractice/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var urlREQ:URLRequest!
+    var sessionDataTask:URLSessionDataTask!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    @IBAction func onBtnTap(_ sender: Any) {
+       
+        let urlString = URL(string: "https://www.brninfotech.com/pulse/modules/admin/ValidateLogin.php")
+        
+        
+        urlREQ = URLRequest(url: urlString!)
+        urlREQ.httpMethod = "POST"
+        
+        let dataToSend = "registeredEmail=kesani.murali@gmail.com&registeredPassword=munna341820&funcName=verifyLogin"
+        urlREQ.httpBody = dataToSend.data(using: String.Encoding.utf8)
+        sessionDataTask = URLSession.shared.dataTask(with: urlREQ) { (data, response, error) in
+            
+            print(self.sessionDataTask!)
+            print("Got Response")
+            do{
+                var converting2JSON  = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
+                print("*********\(converting2JSON)***********")
+                
+            }catch{
+                print("something wrong")
+            }
+        }
+        sessionDataTask.resume()
+        
+        
+    }
+    
 
 }
 
